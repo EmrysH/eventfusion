fe6b67813262cd63b175b7fc760a77ae43bb1291
Fuji <112674895+EmrysH@users.noreply.github.com>
2023-08-08 13:48:41 +0800

    Create RGB_T_Fusion.py
    Optris and RealsenseD455 Fusion

---
+++ RGB_T_Fusion.py
@@ -0,0 +1,252 @@
+
+## import necessary module 
+import cv2
+import numpy as np
+from numpy.lib.format import _filter_header
+
+## import realsense module
+import pyrealsense2 as rs
+
+## import IR module
+import pyOptris as optris
+
+# cv2.namedWindow('fusion image', cv2.WINDOW_NORMAL)
+
+img_num = 0
+
+DLL_path = "/usr/lib/libirdirectsdk.so"
+optris.load_DLL(DLL_path)
+optris.usb_init("22092003.xml")
+
+
+## read the datas ##
+# RGBD data
+rgbIntrinsic = np.load("/home/emrys/Project/Calibration_Pics/RGB_T/Data/optris_rs_RGB_intrinsic.npy")
+rgbDistortion = np.load("/home/emrys/Project/Calibration_Pics/RGB_T/Data/optris_rs_RGB_distortion.npy")
+
+# thermal data
+thermalIntrinsic = np.load("/home/emrys/Project/Calibration_Pics/RGB_T/Data/optris_rs_Thermal_intrinsic.npy")
+thermalDistortion = np.load("/home/emrys/Project/Calibration_Pics/RGB_T/Data/optris_rs_Thermal_distortion.npy")
+
+# rotation and translation data
+relative_R = np.load("/home/emrys/Project/Calibration_Pics/RGB_T/Data/optris_rs_Ralative_rotation_matrix.npy")
+relative_T = np.load("/home/emrys/Project/Calibration_Pics/RGB_T/Data/optris_rs_Ralative_translation_matrix.npy")
+trans_mat = np.c_[relative_R, relative_T]  # creat relative translation matrix 
+relative_Rvct = (cv2.Rodrigues(relative_R))[0]
+## read image of RGB and thermal camera and to get size of images
+rgbImage = cv2.imread('/home/emrys/Project/Calibration_Pics/RGB_T/RGB/RAW/0.png')
+rgbImage_g = cv2.cvtColor(rgbImage, cv2.COLOR_BGR2GRAY)  # graylize
+thermalImage = cv2.imread('/home/emrys/Project/Calibration_Pics/RGB_T/Thermal/RAW/0.png')
+# thermalImage_g = cv2.cvtColor(thermalImage, cv2.COLOR_BGR2GRAY)   #graylize
+# rgbShape = rgbImage_g.shape[::-1]
+# thermalShape = thermalImage.shape[::-1]
+
+## setup realsense
+# Configure depth and color streams
+pipeline = rs.pipeline()
+config = rs.config()
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 15)
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 15)
+
+
+# Start streaming
+profile = pipeline.start(config)
+## boson camera
+
+
+optris.set_palette(3)
+w_t, h_t = optris.get_thermal_image_size()
+w_p, h_p = optris.get_palette_image_size()
+
+
+# Getting the depth sensor's depth scale (see rs-align example for explanation)
+depth_sensor = profile.get_device().first_depth_sensor()
+depth_scale = depth_sensor.get_depth_scale()
+# print("Depth Scale is: " , depth_scale)
+
+# Create an align object
+# rs.align allows us to perform alignment of depth frames to others frames
+# The "align_to" is the stream type to which we plan to align depth frames.
+align_to = rs.stream.color
+align = rs.align(align_to)
+
+
+def get_rgbd_stream():  # with depth and color aligned
+    # Wait for a coherent pair of frames: color
+    pc = rs.pointcloud()
+    points = rs.points()
+    frames = pipeline.wait_for_frames()
+    aligned_frames = align.process(frames)
+    aligned_depth_frame = aligned_frames.get_depth_frame()  # aligned_depth_frame is a 640x480 depth image
+    color_frame = aligned_frames.get_color_frame()
+    depth_image = np.asanyarray(aligned_depth_frame.get_data())
+    # depth_image = np.multiply(depth_scale,depth_image)
+    color_image = np.asanyarray(color_frame.get_data())
+
+    pc.map_to(color_frame)
+    points = pc.calculate(aligned_depth_frame)
+    colorful = color_image.reshape(-1, 3)
+    vtx = np.asanyarray(points.get_vertices())
+    # for i in range(len(vtx)):
+    #     pcdata = [float(vtx[i][0]) * 1000, float(vtx[i][1]) * 1000, float(vtx[i][2]) * 1000,
+    #               float(colorful[i][0]), float(colorful[i][1]), float(colorful[i][2])]
+
+    #    with open('cloud.txt','w') as f:
+    #        for i in range(len(vtx)):
+    #            f.write(str(np.float(vtx[i][0])*1000)+' '+str(np.float(vtx[i][1])*1000)+' '+str(np.float(vtx[i][2])*1000)+' '+str(np.float(colorful[i][0]))+' #'+str(np.float(colorful[i][1]))+' '+str(np.float(colorful[i][2]))+'\n')
+
+    # return rgb image
+    return depth_image, color_image
+
+# Palette:384*288 Thermal: 382*288
+def get_thermal_stream():
+    thermal_img_raw = optris.get_thermal_image(w_t, h_t) # get raw thermal data ,16bit, Kelvin temperature scale
+    thermal_img_gray = optris.get_palette_image(w_p, h_p)
+    thermal_img_gray = cv2.cvtColor(thermal_img_gray, cv2.COLOR_BGR2GRAY)
+
+    # Rescale to 8 bit # normalize raw data
+    # thermal_img_gray = (255*(thermal_img_raw - thermal_img_raw.min())/(thermal_img_raw.max()-thermal_img_raw.min())).astype(np.uint8)
+
+    thermal_img = cv2.resize(thermal_img_gray, (640, 480))
+
+
+    thermal_data = cv2.resize(thermal_img_raw, (640, 480))
+
+
+
+
+
+    return thermal_img, thermal_data
+
+
+
+
+## from pixel coordinate to image coordinate
+fx = rgbIntrinsic[0, 0]
+fy = rgbIntrinsic[1, 1]
+cx = rgbIntrinsic[0, 2]
+cy = rgbIntrinsic[1, 2]
+x = np.arange(0, 640)
+
+y = np.arange(0, 480)
+
+x_c = np.tile(x, (480, 1))
+
+y_c = np.tile(y, (640, 1))
+
+y_c = np.transpose(y_c)
+x_real_imgplane = (x_c - cx) / fx
+y_real_imgplane = (y_c - cy) / fy
+
+
+# undistort image
+def undist (frame, mtx, dist,imsize):
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, imsize, 1)## change the value 1 (with black bounder)or 0 (remove black bounder),
+    udst = cv2.undistort(frame, mtx, dist, None, newcameramtx)
+    return udst
+
+if __name__ == "__main__":
+    # try :
+
+    while True:
+        thermalImage, data = get_thermal_stream()  # get thermal image
+        depthdata, rgbImage = get_rgbd_stream()  # get aligned RGBD image
+
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depthdata, alpha=0.03),
+                                           cv2.COLORMAP_JET)  # convert depthdata to depth image
+        RGBImage = rgbImage.copy()
+
+
+        # ud_thermal = undist(thermalImage, thermalIntrinsic, thermalDistortion, (480, 640))
+        # cv2.imshow("1",ud_thermal)
+        # cv2.waitKey(1)
+
+
+        ## aligned thermal image with RGB and depth image
+        w_x = np.multiply(x_real_imgplane, depthdata)
+        w_y = np.multiply(y_real_imgplane, depthdata)
+        real_point = np.stack((w_x, w_y, depthdata), axis=-1)
+
+        real_point = real_point.reshape(307200, 3)
+
+        bad_obj_points = np.where(real_point[:, 2] == 0)
+
+        # thermal_1D = thermalImage.reshape(307200)
+
+        imagePoints, jacobian = cv2.projectPoints(real_point, relative_Rvct, relative_T, thermalIntrinsic,
+                                                  thermalDistortion)
+
+
+
+
+
+        imagePoints = imagePoints[:, 0, :]
+        imagePoints = np.round((imagePoints).astype(int))
+        imagePoints = np.flip(imagePoints, axis=1)
+
+
+
+
+        row = imagePoints[:, 0]
+        row[row >= 479] = np.array([479])
+        # row[row >= 287] = np.array([287])
+        row[row < 0] = np.array([0])
+        col = imagePoints[:, 1]
+        col[col >= 639] = np.array([639])
+        # col[col >= 383] = np.array([383])
+        col[col < 0] = np.array([0])
+
+        projected_image = thermalImage[row, col]
+        # projected_image = ud_thermal[row, col]
+        projected_image[bad_obj_points] = 1
+        projected_image = 255 - (projected_image.reshape(480, 640))
+
+
+
+        # projected_img_color_1 = cv2.applyColorMap(projected_image, cv2.COLORMAP_HOT)
+        # projected_img_color = cv2.cvtColor(projected_img_color_1,cv2.COLOR_RGB2BGR)
+        ret, mask = cv2.threshold(projected_image, 0, 255, cv2.THRESH_BINARY)
+        fusion_img = cv2.bitwise_not(projected_image, rgbImage, mask=mask)
+
+
+        # cut wrong edge
+        fusion_img_cut = fusion_img[102:378, 135:503]  # 3 time ,4 times
+        RGBImage_cut = RGBImage[102:378, 135:503]  # the same
+        depth_colormap_cut = depth_colormap[102:378, 135:503]
+
+
+        fusion_img_cut_JET = cv2.applyColorMap(fusion_img_cut, cv2.COLORMAP_JET)
+        # fusion_img_JET = cv2.applyColorMap(fusion_img, cv2.COLORMAP_JET)
+
+
+        real_fus_img = cv2.addWeighted(RGBImage_cut, 0.5, fusion_img_cut_JET, 0.5, 0)
+
+
+        # real_fus_img = cv2.addWeighted(RGBImage, 0.5, fusion_img_JET, 0.5, 0)
+
+        thermal_color_resized = cv2.applyColorMap(cv2.resize(thermalImage[:, :], (368, 276)), cv2.COLORMAP_JET)
+        cv2.namedWindow('fusion image', 0)
+        cv2.resizeWindow('fusion image', 640, 480)
+
+
+        cv2.imshow("fusion image",real_fus_img)
+        # cv2.imshow("depth image", depth_colormap_cut)
+        # cv2.imshow("thermal image", thermal_color_resized)
+        # cv2.imshow("RGB image", RGBImage_cut)
+        # cv2.imshow("RGB image", RGBImage)
+
+
+        key = cv2.waitKey(10) & 0xFF
+        if key & 0xFF == 27:
+            break
+
+    # finally :
+    # Stop streaming
+    pipeline.stop()
+    img_num = 0
+    # shutdown thermal camera
+    optris.terminate()
+    # close all windows
+    cv2.destroyAllWindows()
+    print("close all successfully, ending......")
+
