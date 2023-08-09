f94a56d45d8d88e1b465796d506a2f02359d817e
Fuji <112674895+EmrysH@users.noreply.github.com>
2023-08-09 17:04:09 +0800

    Update RGB_T_Enhanced.py

--- RGB_T_Enhanced.py
+++ RGB_T_Enhanced.py
@@ -11,6 +11,7 @@ import pyrealsense2 as rs
 import pyOptris as optris
 
 import threading
+import time
 
 # cv2.namedWindow('fusion image', cv2.WINDOW_NORMAL)
 
@@ -44,52 +45,48 @@ thermalImage = cv2.imread('/home/emrys/Project/Calibration_Pics/RGB_T/Thermal/RA
 # rgbShape = rgbImage_g.shape[::-1]
 # thermalShape = thermalImage.shape[::-1]
 
-## setup realsense
-# Configure depth and color streams
-pipeline = rs.pipeline()
-config = rs.config()
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 15)
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 15)
 
 
-# Start streaming
-profile = pipeline.start(config)
-## boson camera
 
 
 optris.set_palette(1)
 w_t, h_t = optris.get_thermal_image_size()
 w_p, h_p = optris.get_palette_image_size()
 
-
 # Getting the depth sensor's depth scale (see rs-align example for explanation)
-depth_sensor = profile.get_device().first_depth_sensor()
-depth_scale = depth_sensor.get_depth_scale()
-# print("Depth Scale is: " , depth_scale)
 
-
-align_to = rs.stream.color
-align = rs.align(align_to)
 
 
 thermal_img = []
 thermal_data = []
+depthdata = []
+RGBImage = []
+
 
 def get_rgbd_stream():  # with depth and color aligned
-    # Wait for a coherent pair of frames: color
-    pc = rs.pointcloud()
-    points = rs.points()
-    frames = pipeline.wait_for_frames()
-    aligned_frames = align.process(frames)
-    aligned_depth_frame = aligned_frames.get_depth_frame()  # aligned_depth_frame is a 640x480 depth image
-    color_frame = aligned_frames.get_color_frame()
-    depth_image = np.asanyarray(aligned_depth_frame.get_data())
-    # depth_image = np.multiply(depth_scale,depth_image)
-    color_image = np.asanyarray(color_frame.get_data())
+    global depthdata, RGBImage
+    while True:
+        pc = rs.pointcloud()
+        points = rs.points()
+        frames = pipeline.wait_for_frames()
+        aligned_frames = align.process(frames)
+        aligned_depth_frame = aligned_frames.get_depth_frame()  # aligned_depth_frame is a 640x480 depth image
+        color_frame = aligned_frames.get_color_frame()
+        depth_image = np.asanyarray(aligned_depth_frame.get_data())
+        # depth_image = np.multiply(depth_scale,depth_image)
+        color_image = np.asanyarray(color_frame.get_data())
 
-    pc.map_to(color_frame)
-    points = pc.calculate(aligned_depth_frame)
-    return depth_image, color_image
+        pc.map_to(color_frame)
+        points = pc.calculate(aligned_depth_frame)
+        depthdata = depth_image
+        rgbImage = color_image
+        RGBImage = np.asanyarray(rgbImage)
+
+
+
+
+
+
 
 # Palette:384*288 Thermal: 382*288
 def get_thermal_stream():
@@ -106,7 +103,56 @@ def get_thermal_stream():
         cols = code_terms[1]
         thermal_img[rows,cols] = 0
 
+def img_process():
+    while True:
+        w_x = x_real_imgplane * depthdata
+        w_y = y_real_imgplane * depthdata
+        real_point = np.stack((w_x, w_y, depthdata), axis=-1)
+        real_point = real_point.reshape(307200, 3)
+        bad_obj_points = np.where(real_point[:, 2] == 0)
+        # thermal_1D = thermalImage.reshape(307200)
 
+        imagePoints, jacobian = cv2.projectPoints(real_point, relative_Rvct, relative_T, thermalIntrinsic,
+                                                  thermalDistortion)
+        imagePoints = imagePoints[:, 0, :]
+        imagePoints = np.round((imagePoints).astype(int))
+        imagePoints = np.flip(imagePoints, axis=1)
+        row = np.clip(imagePoints[:, 0], 0, 479)
+        col = np.clip(imagePoints[:, 1], 0, 639)
+
+        projected_image = thermal_img[row, col]
+
+        # print([row,col])
+
+        projected_image[bad_obj_points] = 0
+
+        projected_image = projected_image.reshape(480, 640, 3)
+
+        mask = cv2.cvtColor(projected_image, cv2.COLOR_BGR2GRAY)
+        # mask = thermalImage
+        _, mask = cv2.threshold(mask, 1, 255, cv2.THRESH_BINARY_INV)
+
+        # Apply the mask to imageA
+        foreground = cv2.bitwise_and(RGBImage, RGBImage, mask=mask)
+
+        # Invert the mask
+        mask_inv = cv2.bitwise_not(mask)
+
+        # Get the background by removing the foreground from imageB
+        background = cv2.bitwise_and(projected_image, projected_image, mask=mask_inv)
+
+        # print(foreground.shape)
+        # print(background.shape)
+
+        # Combine the foreground and background
+        RGB_T_image = cv2.add(foreground, background)
+
+        RGB_T_image = RGB_T_image[102:378, 135:503]
+        cv2.namedWindow("Fusion")
+        cv2.imshow("Fusion", RGB_T_image)
+        key = cv2.waitKey(10) & 0xFF
+        if key & 0xFF == 27:
+            break
 
 
 
@@ -122,102 +168,57 @@ fy = rgbIntrinsic[1, 1]
 cx = rgbIntrinsic[0, 2]
 cy = rgbIntrinsic[1, 2]
 x = np.arange(0, 640)
-
 y = np.arange(0, 480)
-
 x_c = np.tile(x, (480, 1))
-
 y_c = np.tile(y, (640, 1))
-
 y_c = np.transpose(y_c)
 x_real_imgplane = (x_c - cx) / fx
 y_real_imgplane = (y_c - cy) / fy
 
 
 
+
+
+
+
 thermal_stream_thread = threading.Thread(target=get_thermal_stream)
+rgbd_stream_thread = threading.Thread(target=get_rgbd_stream)
+img_process_thread = threading.Thread(target=img_process)
 
 
 if __name__ == "__main__":
     # try :
+    ## setup realsense
+    # Configure depth and color streams
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 15)
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 15)
+    profile = pipeline.start(config)
 
+    depth_sensor = profile.get_device().first_depth_sensor()
+    depth_scale = depth_sensor.get_depth_scale()
+    align_to = rs.stream.color
+    align = rs.align(align_to)
+
+    rgbd_stream_thread.start()
     thermal_stream_thread.start()
 
-    while True:
-        depthdata, rgbImage = get_rgbd_stream()  # get aligned RGBD image
+    print("initallizing....")
+    time.sleep(5)
 
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depthdata, alpha=0.03),
-                                           cv2.COLORMAP_JET)  # convert depthdata to depth image
-        RGBImage = np.asanyarray(rgbImage)
-
-        # RGBImage = cv2.resize(RGBImage, (640, 480))
-
-        ## aligned thermal image with RGB and depth image
-        w_x = np.multiply(x_real_imgplane, depthdata)
-        w_y = np.multiply(y_real_imgplane, depthdata)
-        real_point = np.stack((w_x, w_y, depthdata), axis=-1)
-
-        real_point = real_point.reshape(307200, 3)
-
-        bad_obj_points = np.where(real_point[:, 2] == 0)
-
-        # thermal_1D = thermalImage.reshape(307200)
-
-        imagePoints, jacobian = cv2.projectPoints(real_point, relative_Rvct, relative_T, thermalIntrinsic,
-                                                  thermalDistortion)
-
-        imagePoints = imagePoints[:, 0, :]
-        imagePoints = np.round((imagePoints).astype(int))
-        imagePoints = np.flip(imagePoints, axis=1)
-
-        row = np.clip(imagePoints[:, 0], 0, 479)
-        col = np.clip(imagePoints[:, 1], 0, 639)
+    img_process_thread.start()
 
 
-        projected_image = thermal_img[row,col]
-
-        projected_image[bad_obj_points] = 0
-
-        projected_image = projected_image.reshape(480, 640,3)
-
-        mask = cv2.cvtColor(projected_image, cv2.COLOR_BGR2GRAY)
-        # mask = thermalImage
-        _, mask = cv2.threshold(mask, 1, 255, cv2.THRESH_BINARY_INV)
-
-        # Apply the mask to imageA
-        foreground = cv2.bitwise_and(RGBImage, RGBImage, mask=mask)
-
-        # Invert the mask
-        mask_inv = cv2.bitwise_not(mask)
-
-        # Get the background by removing the foreground from imageB
-        background = cv2.bitwise_and(projected_image,projected_image, mask=mask_inv)
-
-        # print(foreground.shape)
-        # print(background.shape)
 
 
-        # Combine the foreground and background
-        RGB_T_image = cv2.add(foreground, background)
 
-
-        RGB_T_image = RGB_T_image[102:378, 135:503]
-
-
-        cv2.namedWindow("1")
-        cv2.imshow("1",RGB_T_image)
-
-
-        key = cv2.waitKey(10) & 0xFF
-        if key & 0xFF == 27:
-            break
-
-    # finally :
-    # Stop streaming
-    pipeline.stop()
-    img_num = 0
-    # shutdown thermal camera
-    optris.terminate()
-    # close all windows
-    cv2.destroyAllWindows()
-    print("close all successfully, ending......")
+    # # finally :
+    # # Stop streaming
+    # pipeline.stop()
+    # img_num = 0
+    # # shutdown thermal camera
+    # optris.terminate()
+    # # close all windows
+    # cv2.destroyAllWindows()
+    # print("close all successfully, ending......")
