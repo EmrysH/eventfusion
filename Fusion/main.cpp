391e5e4ed0f9e88a39efecc83ae3c29dc489acbf
Fuji <112674895+EmrysH@users.noreply.github.com>
2023-09-05 15:25:13 +0800

    Add files via upload

--- Fusion/main.cpp
+++ Fusion/main.cpp
@@ -83,19 +83,6 @@ void SensorDataObserver::onFrameDataUpdated(CeleX5ProcessedData* pSensorData)
 }
 
 
-//vector<Point2i> Pointf2i(const vector<Point2d>& src) {
-//    vector<Point2i> dst;
-//    dst.reserve(src.size());
-//
-//    // Convert each Point2d to Point2i using std::transform and lambda function
-//    std::transform(src.begin(), src.end(), std::back_inserter(dst),
-//                   [](const Point2d& point) {
-//                       return Point2i(static_cast<int>(std::round(point.x)),
-//                                      static_cast<int>(std::round(point.y)));
-//                   });
-//
-//    return dst;
-//}
 
 
 
@@ -122,10 +109,10 @@ int* gen_bad_points(vector<Point3d> real_point3d){
     return bad_obj_points;
 }
 
-
+Mat projected_image;
 Mat Fusion(vector<Point2i> imagePoints, Mat thermal_data){
 
-    Mat projected_image = Mat(Size(1,307200),CV_8UC3);
+    projected_image = Mat(Size(1,307200),CV_8UC3);
     int row[307200] = {0};
     int col[307200] = {0};
 
@@ -143,26 +130,40 @@ Mat Fusion(vector<Point2i> imagePoints, Mat thermal_data){
     return projected_image;
 }
 
-//Mat testfunc(Mat imagePoints, Mat thermal_data){
-//
-//    Mat projected_image = Mat(Size(1,307200),CV_8UC1);
-//    int row[307200] = {0};
-//    int col[307200] = {0};
-//
-//
-//    for (int i = 0; i < 307200; i++){
-//
-//        row[i] = std::max(0, std::min(imagePoints.at<int>(col[i],row[0]), 479));
-//        col[i] = std::max(0, std::min(imagePoints.at<int>(col[i],row[1]), 639));
-//        projected_image.at<uchar>(i,0) = thermal_data.at<uchar>(col[i],row[i]);
-//
-//    }
-//
-//    projected_image = projected_image.reshape(1, 480);
-//
-//    return projected_image;
-//}
 
+Mat Fusion_E(vector<Point2i> imagePoints, Mat event_data,int bad_obj_points[307200]){
+
+//    Mat projected_image_E = Mat(Size(1,307200),CV_8UC1);
+
+
+//for Eventoff
+    cv::Mat projected_image_E= cv::Mat::zeros(cv::Size(1, 307200), CV_8UC1);
+
+
+//for TS
+//    Mat projected_image_E(cv::Size(1, 307200), CV_8UC1,Scalar(128));
+
+    int row[307200] = {0};
+    int col[307200] = {0};
+
+
+    for (int i = 0; i < 307200; i++){
+
+        row[i] = std::max(0, std::min(imagePoints[i].x, 479));
+        col[i] = std::max(0, std::min(imagePoints[i].y, 639));
+        projected_image_E.at<uchar>(i,0) = event_data.at<uchar>(col[i],row[i]);
+
+        if (bad_obj_points[i] == 1)
+//        {projected_image_E.at<int>(i,0) = 128;  //for TS
+        {projected_image_E.at<int>(i,0) = 0;  //for Eventoff
+        }
+
+    }
+
+    projected_image_E = projected_image_E.reshape(1, 480);
+
+    return projected_image_E;
+}
 
 
 
@@ -182,47 +183,82 @@ vector<Point3d> Mat2Point(Mat &src){
     return dst;
 }
 
+
+uint8_t * pSensorBuffer = new uint8_t[CELEX5_PIXELS_NUMBER];
 Mat event_frame()
 {
-    std::vector<EventData> vecEvent;
-    pCeleX5->getEventDataVector(vecEvent);
-    cv::Mat sae= cv::Mat::zeros(cv::Size(1280, 800), CV_64F);
+//    std::vector<EventData> vecEvent;
+//    pCeleX5->getEventDataVector(vecEvent);
+//
+//    cv::Mat sae= cv::Mat::zeros(cv::Size(1280, 800), CV_64F);
+//
+//    int dataSize = vecEvent.size();
+//
+//    vector<int> p(dataSize);
+//    vector<int> x(dataSize);
+//    vector<int> y(dataSize);
+//    vector<double> ts(dataSize);
+//
+//    double tau = 50e-3 ;
+//
+//    for (int i = 0; i < dataSize; i++) {
+//        p[i] = vecEvent[i].polarity;
+//        x[i] = vecEvent[i].col;
+//        y[i] = vecEvent[i].row;
+//        ts[i] = vecEvent[i].tOffPixelIncreasing * 0.000001;
+//    }
+//
+//    double t_ref = ts.back();
+//
+//    for (int i = 0; i < ts.size(); i++)
+//    {
+//
+//
+//        if (p[i] > 0) {
+//            sae.at<double>(800-y[i]-1,1280-x[i]-1) = exp(-(t_ref - ts[i]) / tau);
+//        }
+//        if (p[i] < 0 ){
+//            sae.at<double>(800-y[i]-1,1280-x[i]-1) = -exp(-(t_ref - ts[i]) / tau);
+//        }
+//
+//    }
+//
+//    cv::normalize(sae, sae, 0, 255, cv::NORM_MINMAX, CV_8U);
+//
+//    flip(sae, sae, 1);
+//    resize(sae,sae,Size(640,480));
+//
+//    return sae;
 
-    int dataSize = vecEvent.size();
-
-    vector<int> p(dataSize);
-    vector<int> x(dataSize);
-    vector<int> y(dataSize);
-    vector<double> ts(dataSize);
-
-    double tau = 50e-3 ;
-
-    for (int i = 0; i < dataSize; i++) {
-        p[i] = vecEvent[i].polarity;
-        x[i] = vecEvent[i].col;
-        y[i] = vecEvent[i].row;
-        ts[i] = vecEvent[i].tOffPixelIncreasing * 0.000001;
-    }
-
-    double t_ref = ts.back();
-
-    for (int i = 0; i < ts.size(); i++)
-    {
 
 
-        if (p[i] > 0) {
-            sae.at<double>(800-y[i]-1,1280-x[i]-1) = exp(-(t_ref - ts[i]) / tau);
-        }
-        if (p[i] < 0 ){
-            sae.at<double>(800-y[i]-1,1280-x[i]-1) = -exp(-(t_ref - ts[i]) / tau);
-        }
 
-    }
+//    pCeleX5->getFullPicBuffer(pSensorBuffer); //full pic
+//    cv::Mat matFullPic(800, 1280, CV_8UC1, pSensorBuffer);
+//
+//
+//    resize(matFullPic,matFullPic,Size(640,480));
+//    return matFullPic;
 
-    cv::normalize(sae, sae, 0, 255, cv::NORM_MINMAX, CV_8U);
+    pCeleX5->getEventPicBuffer(pSensorBuffer, CeleX5::EventBinaryPic);
+    cv::Mat event(800, 1280, CV_8UC1, pSensorBuffer);
+//
+//    cv::Mat mask;
+//    cv::threshold(event, mask, 254, 255, cv::THRESH_BINARY_INV);
+//
+//    // Create a RGB version of the gray image
+//    cv::Mat rgbImage;
+//    cv::cvtColor(event, rgbImage, cv::COLOR_GRAY2RGB);
+//
+//    // Set the white pixels to green in the RGB image using the mask
+//    rgbImage.setTo(cv::Scalar(0, 255, 0), mask);
+//
+//    event = rgbImage;
 
-    flip(sae, sae, 1);
-    return sae;
+    resize(event,event,Size(640,480));
+    return event;
+
+
 
 }
 
@@ -269,56 +305,68 @@ Mat optris_frame()
 
 }
 
+string dir_path = "../calib/";
+Mat readxml(string name,string index){
+    std::string path = dir_path + name + ".xml";
+    cv::FileStorage file(path,cv::FileStorage::READ);
+    if(!file.isOpened()){
+        std::cout<<"Failed to open the xml file "<<path<<std::endl;
+    }
+    cv::Mat rst;
+    file[index]>>rst;
+    file.release();
+    //std::cout << name << "is:" << rst << std::endl;
+    return rst;
+}
 
 
-cv::Mat rgbIntrinsic, rgbDistortion, thermalIntrinsic, thermalDistortion, relative_R, relative_T,trans_mat,relative_Rvct;
+cv::Mat rgbIntrinsic, rgbDistortion, rgbIntrinsic_E,rgbDistortion_E,thermalIntrinsic, thermalDistortion, eventIntrinsic,eventDistortion,relative_R, relative_T,relative_R_Event, relative_T_Event,trans_mat,relative_Rvct,trans_mat_Event,relative_Rvct_Event;
 
 int main(int argc, char * argv[]) {
 
-    cv::FileStorage fs0("/home/emrys/Project/Calibration_Pics/RGB_T/Data/optris_rs_RGB_intrinsic.xml",
-                        cv::FileStorage::READ);
-    cv::FileStorage fs1("/home/emrys/Project/Calibration_Pics/RGB_T/Data/optris_rs_RGB_distortion.xml",
-                        cv::FileStorage::READ);
-    cv::FileStorage fs2("/home/emrys/Project/Calibration_Pics/RGB_T/Data/optris_rs_Thermal_intrinsic.xml",
-                        cv::FileStorage::READ);
-    cv::FileStorage fs3("/home/emrys/Project/Calibration_Pics/RGB_T/Data/optris_rs_Thermal_distortion.xml",
-                        cv::FileStorage::READ);
-    cv::FileStorage fs4("/home/emrys/Project/Calibration_Pics/RGB_T/Data/optris_rs_Ralative_rotation_matrix.xml",
-                        cv::FileStorage::READ);
-    cv::FileStorage fs5("/home/emrys/Project/Calibration_Pics/RGB_T/Data/optris_rs_Ralative_translation_matrix.xml",
-                        cv::FileStorage::READ);
 
 
-    fs0["rgbIntrinsic"] >> rgbIntrinsic;
-    fs1["rgbDistortion"] >> rgbDistortion;
-    fs2["thermalIntrinsic"] >> thermalIntrinsic;
-    fs3["thermalDistortion"] >> thermalDistortion;
-    fs4["relative_R"] >> relative_R;
-    fs5["relative_T"] >> relative_T;
-//    std::cout << relative_T << std::endl;
-    fs0.release();
-    fs1.release();
-    fs2.release();
-    fs3.release();
-    fs4.release();
-    fs5.release();
+
+
+//    rgbIntrinsic = readxml("optris_rs_RGB_intrinsic","rgbIntrinsic");
+//    rgbDistortion = readxml("optris_rs_RGB_distortion","rgbDistortion");
+
+
+
+    rgbIntrinsic = readxml("event_realsense_RGB_intrinsic","rgbIntrinsic");
+    rgbDistortion = readxml("event_realsense_RGB_distortion","rgbDistortion");
+
+    thermalIntrinsic = readxml("optris_rs_Thermal_intrinsic","thermalIntrinsic");
+    thermalDistortion = readxml("optris_rs_Thermal_distortion","thermalDistortion");
+
+    eventIntrinsic = readxml("event_realsense_event_intrinsic","eventIntrinsic");
+    thermalDistortion = readxml("event_realsense_event_distortion","eventDistortion");
+
+
+    relative_R_Event = readxml("event_rgb_Relative_rotation_matrix","relative_R");
+    relative_T_Event = readxml("event_rgb_Relative_translation_matrix","relative_T");
+
+    hconcat(relative_R_Event, relative_T_Event, trans_mat_Event);
+    Rodrigues(relative_R_Event, relative_Rvct_Event);
+
+
+    relative_R = readxml("optris_rs_Ralative_rotation_matrix","relative_R");
+    relative_T = readxml("optris_rs_Ralative_translation_matrix","relative_T");
 
     hconcat(relative_R, relative_T, trans_mat);
     Rodrigues(relative_R, relative_Rvct);
 
 //    std::cout << thermalDistortion << std::endl;
 
-
-
 Mat rgbImage = imread("/home/emrys/Project/Calibration_Pics/RGB_T/RGB/RAW/0.png");
 
 Mat rgbImage_g;
 
-    cvtColor(rgbImage,rgbImage_g,COLOR_BGR2GRAY);
+cvtColor(rgbImage,rgbImage_g,COLOR_BGR2GRAY);
 
 Mat thermalImage = imread("/home/emrys/Project/Calibration_Pics/RGB_T/Thermal/RAW/0.png");
 
-
+Mat eventImage = imread("/home/emrys/Project/Calibration_Pics/RGB_E/Event/RAW/0.png");
 
 
 //    std::cout << "Shape: (" << rows << " rows, " << cols << " columns)" << std::endl;
@@ -363,15 +411,14 @@ Mat thermalImage = imread("/home/emrys/Project/Calibration_Pics/RGB_T/Thermal/RA
     Mat x_real,y_real;
     eigen2cv(x_real_imgplane,x_real);
 
-//    cout<<x_real_imgplane<<endl;
+
     MatrixXd y_real_imgplane = (y_c.array() - cy) / fy;
     eigen2cv(y_real_imgplane,y_real);
 
     x_real.convertTo(x_real,CV_64FC1);
     y_real.convertTo(y_real,CV_64FC1);
 
-//
-//    cout<<x_real_imgplane<<endl;
+
 
 
 
@@ -383,11 +430,15 @@ Mat thermalImage = imread("/home/emrys/Project/Calibration_Pics/RGB_T/Thermal/RA
 
     pCeleX5->openSensor(CeleX5::CeleX5_MIPI);
     pCeleX5->setFpnFile(FPN_PATH);
-//    pCeleX5->setSensorFixedMode(CeleX5::Event_Off_Pixel_Timestamp_Mode);
-    pCeleX5->setSensorFixedMode(CeleX5::Event_Intensity_Mode);
-    pCeleX5->disableFrameModule();
-    pCeleX5->disableIMUModule();
-    pCeleX5->disableEventCountSlice();
+
+
+//    pCeleX5->setSensorFixedMode(CeleX5::Event_Intensity_Mode);
+    pCeleX5->setSensorFixedMode(CeleX5::Event_Off_Pixel_Timestamp_Mode);
+//    pCeleX5->setSensorFixedMode(CeleX5::Full_Picture_Mode);
+//    pCeleX5->disableFrameModule();
+//    pCeleX5->disableIMUModule();
+//    pCeleX5->disableEventCountSlice();
+//    pCeleX5->setEventFrameTime(8333);
     SensorDataObserver* pSensorData = new SensorDataObserver(pCeleX5->getSensorDataServer());
 
 
@@ -401,35 +452,27 @@ Mat thermalImage = imread("/home/emrys/Project/Calibration_Pics/RGB_T/Thermal/RA
     sigaction(SIGQUIT, &sig_action, NULL); // 3
     sigaction(SIGKILL, &sig_action, NULL); // 9
     sigaction(SIGTERM, &sig_action, NULL); // 15
+    pCeleX5->setRotateType(2);
 
 //    sleep(3);
 
 
 ////    Initializing RealSense
-//    cfg.enable_stream(RS2_STREAM_DEPTH, 640, 480, RS2_FORMAT_Z16, 30);
-//    cfg.enable_stream(RS2_STREAM_COLOR, 640, 480, RS2_FORMAT_BGR8, 30);
-//    pip.start(cfg);
-//
-//    namedWindow("RGB_IMG", WINDOW_AUTOSIZE);
-
 
     rs2::colorizer c;
-    // 创建数据管道
+
     rs2::pipeline pipe;
     rs2::config pipe_config;
     pipe_config.enable_stream(RS2_STREAM_DEPTH, 640, 480, RS2_FORMAT_Z16, 30);
     pipe_config.enable_stream(RS2_STREAM_COLOR, 640, 480, RS2_FORMAT_RGB8, 30);
-    // start() 函数返回数据管道的profile
+
     rs2::pipeline_profile profile = pipe.start(pipe_config);
     // rs2::pipeline_profile profile = pipe.start();
 
-    // 使用数据管道的 profile 获取深度图像像素对应于长度单位（米）的转换比例
 
     float depth_scale = get_depth_scale(profile.get_device());
+    rs2_stream align_to = RS2_STREAM_COLOR;
 
-    // 选择彩色图像数据流来作为对齐对象
-    rs2_stream align_to = RS2_STREAM_COLOR; // 对齐的是彩色图，所以彩色图是不变的
-    // // 将深度图对齐到RGB图
     rs2::align align(align_to);
 
 ////    Initializing Optris
@@ -462,13 +505,20 @@ Mat thermalImage = imread("/home/emrys/Project/Calibration_Pics/RGB_T/Thermal/RA
 
     Mat w_x,w_y;
 
-    Mat RGB_IMG, Depth_IMG,Thermal_IMG,Event_IMG,depthdata,RGBImage,jacobian,projected_image,real_point,real_point_depth;
-    cv::Mat RGB_T_image;
+    int* bad_obj_points;
+
+    Mat RGB_IMG, Depth_IMG,Thermal_IMG,Event_IMG,depthdata,RGBImage,jacobian,projected_image,projected_image_Event,real_point,real_point_depth;
+
+    cv::Mat RGB_T_image,RGB_D_T_E_Image;
+
     vector<Point2d> imagePoints;
+    vector<Point2d> imagePoints_Event;
 
     vector<Point3d> real_point3d;
+    vector<Point3d> real_point3d_Event;
 
     vector<Point2i> imagePoints_int;
+    vector<Point2i> imagePoints_int_Event;
 
 
 
@@ -481,18 +531,18 @@ Mat thermalImage = imread("/home/emrys/Project/Calibration_Pics/RGB_T/Thermal/RA
     while (true)
     {
 
-        Thermal_IMG = optris_frame();
+//        Thermal_IMG = optris_frame();
+        Event_IMG = event_frame();
 
         //////////realsense
         rs2::frameset frameset = pipe.wait_for_frames();
 
-//        if (profile_changed(pipe.get_active_profile().get_streams(), profile.get_streams()))
-//        {
-//            // 如果profile发生改变，则更新align对象，重新获取深度图像像素到长度单位的转换比例
-//            profile = pipe.get_active_profile();
-//            align = rs2::align(align_to);
-//            depth_scale = get_depth_scale(profile.get_device());
-//        }
+        if (profile_changed(pipe.get_active_profile().get_streams(), profile.get_streams()))
+        {
+            profile = pipe.get_active_profile();
+            align = rs2::align(align_to);
+            depth_scale = get_depth_scale(profile.get_device());
+        }
         auto processed = align.process(frameset);
         rs2::frame aligned_color_frame = processed.get_color_frame();
         rs2::frame aligned_depth_frame = processed.get_depth_frame();
@@ -530,51 +580,104 @@ Mat thermalImage = imread("/home/emrys/Project/Calibration_Pics/RGB_T/Thermal/RA
         cv::merge(std::vector<cv::Mat>{w_x, w_y, depthdata}, real_point_depth);
         real_point = real_point_depth.reshape(1, 307200);
 
+        real_point3d = Mat2Point(real_point);
+        bad_obj_points = gen_bad_points(real_point3d);
 
 
-//        real_point3d = Mat2Point(real_point);
-//        bad_obj_points = gen_bad_points(real_point3d);
+/////////////////RGB_T_FUSION//////////////////
+//        cv::projectPoints(real_point, relative_Rvct, relative_T, thermalIntrinsic, thermalDistortion, imagePoints);
+//
+//        imagePoints_int = Pointf2i(imagePoints);
 
-
-        cv::projectPoints(real_point, relative_Rvct, relative_T, thermalIntrinsic, thermalDistortion, imagePoints);
-
-
-        imagePoints_int = Pointf2i(imagePoints);
-
-//        convertScaleAbs(imagePoints, imagePoints);
+//        imagePoints_int = Pointf2i(imagePoints);
+//
+//        projected_image = Fusion(imagePoints_int,Thermal_IMG);
+//
+//        cv::Mat mask;
+//        cv::cvtColor(projected_image, mask, cv::COLOR_BGR2GRAY);
+//
+//        cv::threshold(mask, mask, 1, 255, cv::THRESH_BINARY_INV);
+//
+//        cv::Mat foreground;
+//        cv::bitwise_and(RGBImage, RGBImage, foreground, mask);
+//
+//
+//        cv::Mat mask_inv;
+//        cv::bitwise_not(mask, mask_inv);
+//
+//        cv::Mat background;
+//        cv::bitwise_and(projected_image, projected_image, background, mask_inv);
+//        cv::add(foreground, background, RGB_T_image);
 ////
-//        flip(imagePoints, imagePoints, 1);
-
-        projected_image = Fusion(imagePoints_int,Thermal_IMG);
-
-        cv::Mat mask;
-        cv::cvtColor(projected_image, mask, cv::COLOR_BGR2GRAY);
-
-
-        cv::threshold(mask, mask, 1, 255, cv::THRESH_BINARY_INV);
-
-
-        cv::Mat foreground;
-        cv::bitwise_and(RGBImage, RGBImage, foreground, mask);
-
-
-        cv::Mat mask_inv;
-        cv::bitwise_not(mask, mask_inv);
-
-        cv::Mat background;
-        cv::bitwise_and(projected_image, projected_image, background, mask_inv);
-        cv::add(foreground, background, RGB_T_image);
+//        projected_image = Fusion(imagePoints_int,Thermal_IMG);
+//
+//        cv::Mat mask;
+//        cv::cvtColor(projected_image, mask, cv::COLOR_BGR2GRAY);
+//
+//        cv::threshold(mask, mask, 1, 255, cv::THRESH_BINARY_INV);
+//
+//        cv::Mat foreground;
+//        cv::bitwise_and(RGBImage, RGBImage, foreground, mask);
+//
+//
+//        cv::Mat mask_inv;
+//        cv::bitwise_not(mask, mask_inv);
+//
+//        cv::Mat background;
+//        cv::bitwise_and(projected_image, projected_image, background, mask_inv);
+//        cv::add(foreground, background, RGB_T_image);
+        ///////////////////END//////////////////////////////////
 
 
-        cv::Rect roi(135, 102, 368, 276);
-        cv::Mat croppedImage = RGB_T_image(roi);
 
 
-        imshow("Fusion",croppedImage);
-//        imshow("Fusion",projected_image);
+        cv::projectPoints(real_point, relative_Rvct_Event, relative_T_Event, eventIntrinsic, eventDistortion, imagePoints_Event);
+
+        imagePoints_int_Event = Pointf2i((imagePoints_Event));
+
+        projected_image_Event = Fusion_E(imagePoints_int_Event,Event_IMG,bad_obj_points);
+//
+        cv::Mat maskE, Event_BGR;
+
+
+        cv::cvtColor(projected_image_Event, Event_BGR, cv::COLOR_GRAY2BGR);
+
+        cv::cvtColor(Event_BGR, maskE, cv::COLOR_BGR2GRAY);
+
+
+
+        //mask for Eventoff
+        cv::threshold(maskE, maskE, 1, 255, cv::THRESH_BINARY_INV);
+
+
+        //mask for TS
+//        maskE = (maskE>100 & maskE<130);
+
+        cv::Mat foregroundE;
+        cv::bitwise_and(RGBImage, RGBImage, foregroundE, maskE);
+
+
+        cv::Mat mask_invE;
+        cv::bitwise_not(maskE, mask_invE);
+//
+        cv::Mat backgroundE;
+
+
+        cv::bitwise_and(Event_BGR, Event_BGR, backgroundE, mask_invE);
+        cv::add(foregroundE, backgroundE, RGB_D_T_E_Image);
+
+
+
+//        cv::Rect roi(135, 102, 368, 276);
+//        cv::Mat Fusion = projected_image_Event(roi);
+
+
+        imshow("Fusion",RGB_D_T_E_Image);
+//        imshow("Fusion",projected_image_Event);
 //        imshow("Thermal", Thermal_IMG);
 //        imshow("RGB_IMG",RGB_IMG);
 //        imshow("TS Pic", Event_IMG);
+
 
         waitKey(1);
 
